b37e0d29ad399a53047850bc8206615026b4ed09
GitHub Action <action@github.com>
2022-04-12 20:51:27 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/outofculture/refactordeprecate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.outofculture.refactordeprecate.services
+
+import com.github.outofculture.refactordeprecate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/outofculture/refactordeprecate/services/MyProjectService.kt
+++ src/main/kotlin/com/github/outofculture/refactordeprecate/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.outofculture.refactordeprecate.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.outofculture.refactordeprecate.MyBundle
 
 class MyProjectService(project: Project) {
 
